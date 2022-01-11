1f0bd58b384b59831ed3a276d419f52a619ce7d5
mabilweb <h3mabil@gmail.com>
2022-01-11 14:46:37 -0600

    ss

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express =require('express');
 const router = express.Router();
+const appp =express();
 const path =require('path');
+
+appp.set('view engine', 'ejs');
 
 
 router.get('/',(req,res)=>{
