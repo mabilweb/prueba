73c7827b9ae4392dafe73d7296bed9c35501ea76
mabilweb <h3mabil@gmail.com>
2022-01-11 15:00:20 -0600

    ss

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,12 @@ const router = express.Router();
 const path =require('path');
 
 
-router.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname + 'index.ejs'));
+//router.get('/',(req,res)=>{
+    //res.sendFile(path.join(__dirname + 'index.ejs'));
     //res.send('index');
     //res.render('index');
     //console.log("gato barato");
-});
+//});
 
 //router.get('/about',(req,res)=>{
 //    res.sendFile(path.join(__dirname,'views/index.html'));
