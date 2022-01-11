051ca1c96a64c3f2733703f11d6e0af3392d7431
mabilweb <h3mabil@gmail.com>
2022-01-11 06:50:42 -0600

    cambio

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const port =process.env.PORT || 4000;
 
 app.listen(port);
 
-app.get("/", (req,res)=>{
-    res.send("<h1>ladren perros</h1>");
+router.get('/',(req,res)=>{
+    res.sendFile("home.html");
+    
 });
 
 console.log(`Listen on port ${port}`);
