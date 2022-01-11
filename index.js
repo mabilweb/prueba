bbbab067c372e8b04c1617f78f6a545e04b4d130
mabilweb <h3mabil@gmail.com>
2022-01-11 10:52:20 -0600

    cambio

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-const express = require("express");
-const app = express();
+const express = require('express');
+const app =express();
+const path =require('path');
+
+//settings
+app.set('port',4000);
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine', 'ejs');
+
+//middleawres
+
+///
+
+///routes
+app.use(require('./routes/index'));
 
 
-const port =process.env.PORT || 4000;
+///static files
+//app.use(express.static(__dirname + '/public'));
 
-app.listen(port);
 
-
-app.get('/',(req,res)=>{
-    res.sendFile('./mabilweb/prueba/main/views/home.html');
-    
+///listeig the server
+app.listen(app.get('port'), () =>{
+    console.log('sirviendo  en :', app.get('port'));
 });
-
-console.log(`Listen on port ${port}`);
