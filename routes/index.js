d602a8e90f967d8d38ba5bd4bd99e8c0cc642baf
mabilweb <h3mabil@gmail.com>
2022-01-11 11:42:19 -0600

    sdd

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express =require('express');
 const router = express.Router();
 
 router.get('/',(req,res)=>{
-    res.sendFile('index.html');
+    res.render('index.html');
     //res.send('index');
 });
 
