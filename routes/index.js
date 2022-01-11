2a49150275a084a3ab79ebe9f38f02d1496beebb
mabilweb <h3mabil@gmail.com>
2022-01-11 11:54:02 -0600

    ggg

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express =require('express');
 const router = express.Router();
 
 router.get('/',(req,res)=>{
-    console.log(path.join(__dirname));
+    console.log("gato barato");
     //res.render('index')
     //res.send('index');
 });
