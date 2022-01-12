aadeef19bf430ef80c0e4a153efb6324652c1e2d
mabilweb <h3mabil@gmail.com>
2022-01-11 18:20:52 -0600

    dd

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     //console.log(path.join(__dirname , 'views/index.html'));
-    res.sendFile(path.join(__dirname , 'views/index.ejs'));
-    //res.render('index');
+    //res.sendFile(path.join(__dirname , 'views/index.ejs'));
+    res.render('index.ejs');
 });
 
 
