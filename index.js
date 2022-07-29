69cd29ad4afebaec3e5030a35717e67c5098e9bd
mabilweb <h3mabil@gmail.com>
2022-07-28 22:20:08 -0500

    cambio5

--- index.js
+++ index.js
@@ -1,63 +1,25 @@
-const express =require('express');
-const morgan =require('morgan');
-const exphbs=require('express-handlebars');
+const express = require('express');
+const app =express();
 const path =require('path');
-const flash=require('connect-flash');
-const session =require('express-session');
-const MySQLStore=require('express-mysql-session');
-const passport = require('passport');
-
-const {database}=require('./keys');
-const app=express();
-require('./lib/passport');
 
 //settings
-app.set('port',process.env.PORT ||4000);
+app.set('port',4000);
 app.set('views',path.join(__dirname,'views'));
-app.engine('.hbs',exphbs({
-defaultLayout:'main',
-layoutsDir:path.join(app.get('views'),'layouts'),
-partialsDir:path.join(app.get('views'),'partials'),
-extname:'.hbs',
-helpers:require('./lib/handlebars')
-}));
-app.set('view engine','.hbs');
+app.set('view engine', 'ejs');
 
-//middlewares
+//middleawres
 
-//global varials
-app.use(morgan('dev'));
-app.use(express.urlencoded({extended:false}));
-app.use(express.json());
-app.use(session({
-    secret:'faztmysqlnodesession',
-    resave:false,
-    saveUninitialized:false,
-    store: new MySQLStore(database)
-}));
-app.use(flash());
-app.use(passport.initialize());
-app.use(passport.session());
+///
+
+///routes
+app.use(require('./routes/index'));
 
 
-app.use((req,res,next)=>{
-    app.locals.success=req.flash('success');
-    app.locals.message=req.flash('message');
-    app.locals.user = req.user;
-    next();
-    });
-//Routes
-app.use(require('./src/routes'));
-app.use(require('./src/routes/autenthication'));
-app.use('/links',require('./routes/links'));
-
-//Public
-//app.use(express.static('public'))
-app.use(express.static(path.join(__dirname,'public')));
-//app.use(express.static(path.join(__dirname,'public')));
+///static files
+app.use(express.static(__dirname + '/public'));
 
 
-//statir server
-app.listen(app.get('port'),()=>{
-    console.log('server on pedro',app.get('port'));
+///listeig the server
+app.listen(app.get('port'), () =>{
+    console.log('sirviendo  en :', app.get('port'));
 });
